b94091aae5dc648d2ea5ee8df358e64a81ecbb06
Lauren Hess <laurenbethhess@gmail.com>
2021-07-14 12:26:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+
+function writeCards() {
+  const birthday = [];
+  for (let i = 0; i < names.length; i++) {
+    birthday.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+    return birthday;
+}
+
+writeCards();
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+    console.log(countDown--);
+    } 
+  }
+  
+  countDown ();
